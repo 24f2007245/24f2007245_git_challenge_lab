7877370e9a5868cd5b1ec04b857184469f4aba23
[Chhotu Kumar] <[kundanranahzb@gmail.com]>
2025-09-17 22:56:28 +0530

    done with front end

--- routes.py
+++ routes.py
@@ -9,6 +9,11 @@ def index():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
